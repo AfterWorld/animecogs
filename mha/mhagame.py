5f422f1c9dbf1a0ca603a1a38885875e7e3a8c37
AfterWorld <31444028+AfterWorld@users.noreply.github.com>
2024-07-20 02:19:28 -0400

    Update mhagame.py

--- mha/mhagame.py
+++ mha/mhagame.py
@@ -537,9 +537,7 @@ class MHAGame(commands.Cog):
     @checks.is_owner()
     async def mha_admin(self, ctx):
         """MHA game admin commands"""
-        if ctx.invoked_subcommand is None:
-            await ctx.send_help(ctx.command)
-
+        
     @mha_admin.command(name="wipe")
     async def wipe_user_data(self, ctx, user: discord.Member):
         """Wipe a user's MHA game data"""
